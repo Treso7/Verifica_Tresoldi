6c4097c2b648602eccf719ec1fc28937a1820a61
Treso7 <74243638+Treso7@users.noreply.github.com>
2022-01-23 20:39:31 +0100

    secondo commeat>

--- verifica.js
+++ verifica.js
@@ -4,12 +4,22 @@ var fs = require("fs");
 var port = 3000;
 var host = 'localhost';
 a.get('/', (request, response)=>{
-    response.send("<html><head><title>pagina</title></head><body><h1>Pagina principlea</h1><a href='http://localhost:3000/registrazione'>registrazione</a><a href='http://localhost:3000/login'>login</a></body>")
+   response.send("<html><head><title>pagina</title></head><body><h1>Pagina principlea</h1><a href='http://localhost:3000/registrazione'>registrazione</a><a href='http://localhost:3000/login'>login</a></body>")
 });
 a.get('/login', (request, response)=>{
-    response.send("<html><head><title>login</title></head><body><h1>login</h1><form method='get' action='menu'><input type='text', name='email'><input type='text', name='password'><input type='submit', name='manda'></form>);
+ response.send("<html><head><title>login</title></head><body><h1>login</h1><form method='get' action='menu'><input type='text', name='email'><input type='text', name='password'><input type='submit', name='manda'></form>);
 });
 a.get('/menu', (request, response)=>{
-    var nome = request.query.email;
-    var passwoood = request.query.password;
-    response.send("<html><head><title>menu</title></head><body
+   var nome = request.query.email;
+  var passwoood = request.query.password;
+    fs.readFile("salva.json", (err, dati)=>{
+        var scritte = JSON.parse(dati);
+       scritte.forEach(elemento => {
+           if (elemento.username == nome) {
+                 if(elemento.password == passwoood) {
+                     var resposta = "<html>
+                       response.send(
+                    }
+                }
+            });
+   
