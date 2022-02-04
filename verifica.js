62d1e2b1ff0df55947cd08186a1f2ad046a1b771
Treso7 <74243638+Treso7@users.noreply.github.com>
2022-02-04 17:43:13 +0100

    finale

--- verifica.js
+++ verifica.js
@@ -6,12 +6,12 @@ var host = 'localhost';
 
 a.get('/login', (request, response)=>{
    var nome = request.query.email;
-  var passwoood = request.query.password;
-    fs.readFile("salva.json", (err, dati)=>{
+   var password = request.query.password;
+   fs.readFile("salva.json", (err, dati)=>{
         var scritte = JSON.parse(dati);
-       scritte.forEach(elemento => {
+        scritte.forEach(elemento => {
            if (elemento.nome == nome) {
-                 if(elemento.password == passwoood) {
+                 if(elemento.password == password) {
                        response.json({status : "ok"});
                     } else {
                        response.json({status : "no ok"});
@@ -20,13 +20,38 @@ a.get('/login', (request, response)=>{
                    response.json({status : "no ok"});
                 }
             });
-    });
+   });
+});
+   
+a.get('/registra', (request, response)=>{
+   var gruppo = request.query.gruppo;
+   var nome = request.query.nome;
+   fs.readFile("salva.json", (err, dati)=>{
+        var scritte = JSON.parse(dati);
+        for (var i = 0; i < scritte.length; i++) {
+            if(scritte[i].nome == nome) {
+               scritte[i].gruppo = gruppo;
+               response.json({status : "ok"});
+            }
+        }
+      fs.writeFile("salva.json", JSON.stringify(scritte), (err)=>{
+         response.json({cancellato : "no ok"});
+      });
+   });
+});
    
 a.get('/cancella', (request, response)=>{
    fs.readFile("salva.json", (err, dati)=>{
-    var scritte = JSON.parse(dati);
-      scritte = "";
-      fs.writeFile("salva.json", JSON.stringify(scritte), (err)=>{response.json({cancellato : "no ok"});});
-      response.json({cancellato : "ok"});
+      var scritte = JSON.parse(dati);
+      var dacancellare = request.query.nome;
+      for (var i = 0; i < scritte.length; i++) {
+            if(scritte[i].nome == dacancellare) {
+               delete scritte[i]
+               response.json({status : "ok"});
+            }
+        }
+      fs.writeFile("salva.json", JSON.stringify(scritte), (err)=>{
+         response.json({cancellato : "no ok"});
+      });
    });
-   });
+});
